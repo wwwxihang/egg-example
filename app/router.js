cb15c00f2c5cb5fcc6453259b3fd609e902aa50a
吴希航 <wuxh@cloud-young.com>
2019-03-20 20:35:40 +0800

    post

--- app/router.js
+++ app/router.js
@@ -4,13 +4,18 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
+	const { router, controller } = app;
 
-  // app.router.get('/home/index', app.controller.home.index);
-  // app.router.redirect('/', '/home/index', 302);
+	// app.router.get('/home/index', app.controller.home.index);
+	// app.router.redirect('/', '/home/index', 302);
 
-  router.get('/home', controller.home.index);
-  router.get('/user/fetch/:id', controller.user.fetch);
-  router.get('/user', 'user.index');
-  router.get('/search', controller.search.index);
+	router.get('/home', controller.home.index);
+	router.get('/user/fetch/:id', controller.user.fetch);
+	router.get('/user', 'user.index');
+	router.get('/search', controller.search.index);
+
+
+	router.post('createPost', '/api/posts', controller.post.create);
+	router.get('/listPostsGet', controller.post.listPostsGet);
+	router.post('/listPostsPost', controller.post.listPostsPost);
 };
--- config/config.default.js
+++ config/config.default.js
@@ -6,34 +6,40 @@
  * @param {Egg.EggAppInfo} appInfo app info
  */
 module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
-  const config = {};
+    /**
+     * built-in config
+     * @type {Egg.EggAppConfig}
+     **/
+    const config = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1552448015135_6457';
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1552448015135_6457';
 
-  // add your middleware config here
-  config.middleware = ['gzip'];
-  config.gzip = {
-    // enable: false,
-    // match: '/user',
-    match(ctx) {
-      // 只有 ios 设备才开启
-      const reg = /iphone|ipad|ipod|Mac/i;
-      return reg.test(ctx.get('user-agent'));
-    },
-    threshold: 1024, // 小于 1k 的响应体不压缩
-  }
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
-  };
+    // add your middleware config here
+    config.middleware = ['gzip'];
+    config.gzip = {
+        // enable: false,
+        // match: '/user',
+        match(ctx) {
+            // 只有 ios 设备才开启
+            const reg = /iphone|ipad|ipod|Mac/i;
+            return reg.test(ctx.get('user-agent'));
+        },
+        threshold: 1024, // 小于 1k 的响应体不压缩
+    }
 
-  return {
-    ...config,
-    ...userConfig,
-  };
+    config.security = {
+        csrf: {
+            enable: false
+        }
+    }
+    // add your user config here
+    const userConfig = {
+        // myAppName: 'egg',
+    };
+
+    return {
+        ...config,
+        ...userConfig,
+    };
 };
