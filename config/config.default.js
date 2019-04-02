dd8763a93e5d4e487b7eb31b57e8e3cef3a7aa87
吴希航 <wuxh@cloud-young.com>
2019-04-02 12:09:03 +0800

    0.0.0.0

--- config/config.default.js
+++ config/config.default.js
@@ -32,7 +32,7 @@ module.exports = appInfo => {
     config.cluster = {
         listen: {
             port: 7001,
-            hostname: '127.0.0.1',
+            hostname: '0.0.0.0',
             // path: '/var/run/egg.sock',
         }
     }
